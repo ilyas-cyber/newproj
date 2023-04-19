3bd1d4423e9e5d8b4ecd34755ac656f8a87891a0
Muhammad Ilyas <75419324+ilyas-cyber@users.noreply.github.com>
2023-04-19 11:37:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,9 @@ const connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database
+    database: process.env.database,
+     PORT: process.env.PORT
+
 });
 
 // connect to the database
